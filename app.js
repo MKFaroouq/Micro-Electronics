e8aef3e82c0fdd752d582bc78f89d2467654b94e
MKFaroouq <iprobazooka@gmail.com>
2026-02-16 17:48:58 +0200

    Micro-Electronics

--- app.js
+++ app.js
@@ -1,82 +1,258 @@
-require('dotenv').config()
+// require('dotenv').config()
+// const express = require('express')
+// const app = express()
+// const mongoose = require("mongoose")    
+// const bcrypt = require("bcrypt")
+
+
+// app.use(express.json())
+
+// const port = process.env.PORT || 3000
+
+// async function main(){
+//     try{
+//         await mongoose.connect(process.env.MONGO_URL)
+//         console.log("Connected to DB")
+//     }
+//     catch(err){
+//         console.log(err)
+//     }
+// }
+// main()
+
+// // app.listen(port, () => {
+// //   console.log(`listening on port ${port}`)
+// // })
+
+// //dbConnection();
+
+// // require models
+// const User = require("./models/Users")
+
+// app.post("regestire" , async (req , res)=>
+// {
+//     try
+//     {
+//         // get data
+//         const  {username , email , password , role} = req.body
+//         // valitade data
+//         if ( !username || !email || !password || !role ) 
+//         return res.status(400).json({msg:"invaild data"})
+//         // create new user
+//         // response
+//     }
+//     catch(error) 
+//     { 
+        
+//     }
+
+//     app.post("./login" , async(req,res) =>{
+//         try {
+//             const{email , password} req.body;
+//             if(!email || password)
+//                 return res.status(400).json({msg:"missing data"})
+
+//         }
+//     })
+//     catch(error) 
+    
+
+//     const existUser = await User.findOne({email})
+    
+//     if (existUser) 
+//         return res.status(400).json({msg:"account already exist"})
+
+//     const hashpass = await bcrypt.hash(pass,10)
+
+//     const user = await User.create({
+//         username,
+//         email,
+//         password:hashpass,
+//         role,
+//     })
+
+//     res.status(201).json({
+//         msg:"done created user",
+//         data:user,
+//     })
+
+//     app.listen(port, () => {
+//   console.log(`listening on port ${port}`)
+// })
+// })
+
+// omar code // 
+
+/////////
+
+require('dotenv').config();
 const express = require('express')
 const app = express()
-const mongoose = require("mongoose")    
-const bcrypt = require("bcrypt")
+const port = process.env.PORT
+const  bcrypt =require("bcrypt")
+const User = require('./models/User')
+const Product = require('./models/Products')
+const mongoose =require('mongoose')
 
 
 app.use(express.json())
 
-const port = process.env.PORT || 3000
-
-async function main(){
+async function main() {
     try{
-        await mongoose.connect(process.env.MONGO_URL)
-        console.log("Connected to DB")
+    await mongoose.connect(process.env.MONGO_URL)
+    console.log("connected")
     }
-    catch(err){
-        console.log(err)
+    catch(error){
+        console.log(error)
     }
 }
+
 main()
 
-// app.listen(port, () => {
-//   console.log(`listening on port ${port}`)
-// })
 
-//dbConnection();
 
-// require models
-const User = require("./models/Users")
 
-app.post("regestire" , async (req , res)=>
-{
-    try
-    {
-        // get data
-        const  {username , email , password , role} = req.body
-        // valitade data
-        if ( !username || !email || !password || !role ) 
-        return res.status(400).json({msg:"invaild data"})
-        // create new user
-        // response
+
+
+
+
+
+app.post("/register",async(req,res)=>{
+
+    try{
+   const {name,email,password,role}=req.body;
+
+   if(!name||!email||!password)
+   return res.status(400).json({msg:"missing data"})
+
+   const existUser = await User.findOne({email});
+   if(existUser)return res.status(400).json({msg:"wrong data"});
+
+   const hashPassword = await bcrypt.hash(password,10);
+
+
+   const user =await User.create({
+       name,
+       email,
+       password:hashPassword,
+       role
+   });
+
+   res.status(201).json({
+     msg:"done created User",
+     data:user,
+   });
+
     }
-    catch(error) 
-    { 
-        
+
+    catch(error){
+
+        console.log(error);
     }
 
-    app.post("./login" , async(req,res) =>{
-        try {
-            const{email , password} req.body;
-            if(!email || password)
-                return res.status(400).json({msg:"missing data"})
 
-        }
-    })
-    catch(error) 
-    
 
-    const existUser = await User.findOne({email})
-    
-    if (existUser) 
-        return res.status(400).json({msg:"account already exist"})
+});
 
-    const hashpass = await bcrypt.hash(pass,10)
+app.post ("/login", async(req,res)=>{
 
-    const user = await User.create({
-        username,
-        email,
-        password:hashpass,
-        role,
-    })
+try{
+ 
+const {email,password}=req.body;
 
-    res.status(201).json({
-        msg:"done created user",
-        data:user,
-    })
+if (!email||!password)
+return res.status(400).json({msg:"missing data"});
 
-    app.listen(port, () => {
-  console.log(`listening on port ${port}`)
+const user = await User.findOne({email});
+   if(!user)return res.status(404).json({msg:"your accont not found"});
+
+
+   const matchPassword = await bcrypt.compare(password,user.password);
+   if(!matchPassword)return res.status(400).json({msg:"invalid password"});
+   
+  res.status(200).json({
+      msg: "login success",
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+      }
+    });
+}
+
+  catch(error){
+
+        console.log(error);
+    }
+
+
+
 })
+
+
+
+// product code 
+
+app.post("/products",async(req,res)=>{
+    try{
+const {title,price,stoke}=req.body
+if(!title || !price || !stoke) return res.status(401).json({
+    msg:"must add all data"
+})
+const product = await Product.create({
+title,price,stoke
+});
+
+    // valitade data with if exist
+const existProduct = await title.findOne({product});
+   if(existProduct) return res.status(400).json({msg:"already exist"});
+
+
+res.status(201).json({
+    msg:"created",
+    data:product
+
+
+
+
+})
+
+    }
+    catch(error){
+        console.log(error)
+        res.status(500).json({
+msg:"server error -m-"
+        })
+    }
+})
+
+app.get("/products" , async(req , res)=>{
+    try{
+        const product =await Product.find()
+        res.status(401).json({
+            msg:"finded",
+            data:product
+        })
+
+    }
+    catch(error){
+        console.log(error)
+        res.status(500).json({
+            msg:"server error"
+        })
+    }
+})
+
+
+
+
+
+
+
+
+
+
+
+app.listen(port,()=>{
+    console.log(`connected with${port}`)
 })
